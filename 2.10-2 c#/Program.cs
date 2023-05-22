394587514b824e9b5577552b1dabfdc0f831ef6b
garnello <118890237+garnello@users.noreply.github.com>
2023-05-23 01:03:37 +0700

    Add files via upload

---
+++ 2.10-2 c#/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2._10
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the value of the triangle A:");
+            double FitrstSide = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of the triangle B:");
+            double SecondSide = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of the triangle C:");
+            double ThirdSide = double.Parse(Console.ReadLine());
+
+            Triangle triangle = new Triangle(FitrstSide, SecondSide, ThirdSide);
+            while (true)
+            {
+                Console.WriteLine(
+                "1.Find perimeter.\n" +
+                "2.Find square.\n" +
+                "3.Median intersection point.\n" +
+                "4.Exit\n");
+                int choise = int.Parse(Console.ReadLine());
+
+                switch (choise)
+                {
+                    case 1:
+                        Console.WriteLine(triangle.Perimeter());
+                        break;
+
+                    case 2:
+                        Console.WriteLine(triangle.Square());
+                        break;
+
+                    case 3:
+                        Console.WriteLine(triangle.GetMedianIntersection());
+                        break;
+
+                    case 4:
+                        return;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ 2.10-2 c#/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2._10
+{
+    public class Triangle
+    {
+        public double FirstSide { get; set; }
+        public double SecondSide { get; set; }
+        public double ThirdSide { get; set; }
+
+        public Triangle(double FirstSide, double SecondSide, double ThirdSide)
+        {
+            this.FirstSide = FirstSide;
+            this.SecondSide = SecondSide;
+            this.ThirdSide = ThirdSide;
+        }
+
+        public double Perimeter()
+        {
+            return FirstSide + SecondSide + ThirdSide;
+        }
+
+        public double Square()
+        {
+            double semiPerimeter = Perimeter() / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - FirstSide) * (semiPerimeter - SecondSide) * (semiPerimeter - ThirdSide));
+        }
+
+        public Tuple<double, double> GetMedianIntersection()
+        {
+
+            double x = (FirstSide + SecondSide + ThirdSide) / 3;
+            double y = Square() * 2 / (FirstSide + SecondSide + ThirdSide);
+            return Tuple.Create(x, y);
+
+        }
+    }
+}
